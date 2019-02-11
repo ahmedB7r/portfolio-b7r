7b97881a637a9d74473a21a6d3dbce46db6b0400
ahmedB7r <ahmedmagdyb7r@gmail.com>
2019-02-11 21:22:49 +0200

    Updates

---
+++ precache-manifest.ca3f5a2040f70cf9e3d7805fec1608c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abe02c9dfc990d520a56",
+    "url": "/B7r/static/js/runtime~main.abe02c9d.js"
+  },
+  {
+    "revision": "b7952aea1e1a9a2f9b5a",
+    "url": "/B7r/static/js/main.b7952aea.chunk.js"
+  },
+  {
+    "revision": "5e44d5c25d924cd35418",
+    "url": "/B7r/static/js/2.5e44d5c2.chunk.js"
+  },
+  {
+    "revision": "b7952aea1e1a9a2f9b5a",
+    "url": "/B7r/static/css/main.8653f3d5.chunk.css"
+  },
+  {
+    "revision": "5e44d5c25d924cd35418",
+    "url": "/B7r/static/css/2.e8c9ecd9.chunk.css"
+  },
+  {
+    "revision": "629f3f1952ebc446377af74eb8e1d2cd",
+    "url": "/B7r/index.html"
+  }
+];
