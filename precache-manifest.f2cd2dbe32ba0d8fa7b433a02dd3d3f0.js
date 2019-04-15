27f2e0077075d8bc5797f2f44258980a9cd53f80
ahmedB7r <ahmedmagdyb7r@gmail.com>
2019-04-15 13:46:57 +0200

    Updates

--- precache-manifest.f2cd2dbe32ba0d8fa7b433a02dd3d3f0.js
+++ precache-manifest.f2cd2dbe32ba0d8fa7b433a02dd3d3f0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio-b7r/static/js/runtime~main.6ad5c789.js"
   },
   {
-    "revision": "6424f562d1db81691b74",
-    "url": "/portfolio-b7r/static/js/main.6424f562.chunk.js"
+    "revision": "224e584f01099b76f880",
+    "url": "/portfolio-b7r/static/js/main.224e584f.chunk.js"
   },
   {
     "revision": "6628b86b9a194cdede75",
     "url": "/portfolio-b7r/static/js/2.6628b86b.chunk.js"
   },
   {
-    "revision": "6424f562d1db81691b74",
+    "revision": "224e584f01099b76f880",
     "url": "/portfolio-b7r/static/css/main.60a22060.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio-b7r/static/css/2.d17142df.chunk.css"
   },
   {
-    "revision": "fd5e54a2c1a4e32e2442f56764f376b6",
+    "revision": "455d747f827d2fe49b6b072f130b12c4",
     "url": "/portfolio-b7r/index.html"
   }
 ];
