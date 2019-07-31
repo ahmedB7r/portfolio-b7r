352967672da0d65c7a154a2c852ead4fbeb4f25e
ahmedB7r <ahmedmagdyb7r@gmail.com>
2019-08-01 00:25:21 +0200

    Updates

--- precache-manifest.adb4be778efb3552911230cc9e39e25d.js
+++ precache-manifest.adb4be778efb3552911230cc9e39e25d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio-b7r/static/js/runtime~main.d8691931.js"
   },
   {
-    "revision": "ca89fee3dff02ab789b3",
-    "url": "/portfolio-b7r/static/js/main.98f6d665.chunk.js"
+    "revision": "14343d7668f0a107ae5a",
+    "url": "/portfolio-b7r/static/js/main.37fc8141.chunk.js"
   },
   {
     "revision": "ee97cf352187340c2b69",
     "url": "/portfolio-b7r/static/js/2.b5b190d0.chunk.js"
   },
   {
-    "revision": "ca89fee3dff02ab789b3",
+    "revision": "14343d7668f0a107ae5a",
     "url": "/portfolio-b7r/static/css/main.9a2958c7.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio-b7r/static/css/2.8bb73346.chunk.css"
   },
   {
-    "revision": "636c671daf83bd77607a11c8702bf82d",
+    "revision": "6eaf977b1fbb37248327c112a29f0fd0",
     "url": "/portfolio-b7r/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-b7r/precache-manifest.9fd015174602ca8f07ebbdc0a55d1536.js"
+  "/portfolio-b7r/precache-manifest.adb4be778efb3552911230cc9e39e25d.js"
 );
 
 workbox.clientsClaim();
